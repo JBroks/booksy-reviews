7ecee6e69527b7a01661f3e1662f8133d25c676e
Joanna Broks <joanna.broks@gmail.com>
2019-10-25 12:12:35 +0000

    index.html and base.html added to templates

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 import os
 # Import Flask functionality in order to set up the application for use
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 
 # Create an instance of Flask / Flask app and store it in the app variable
 app = Flask(__name__)
 
-# Use the OS library to set a constant called MONGO_URI
-# by using the getenv() method to read in the environment variable that we just set.
-# Set another constant called MONGO_DBNAME and give that the name of our database
-
-MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DBNAME = "booksyDB"
+# Configure MONGO_DBNAME and MONGO_URI and pass it via os environment
+app.config['MONGO_DBNAME'] = 'booksDB'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
 
 # Set up the connection to the booksyDB database
 
-mongo = PyMongo
+mongo = PyMongo(app)
 
 # Create a function with a route in it
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 # Set up IP address and port number so that AWS how to run and where to run the application 
 if __name__ == '__main__':
