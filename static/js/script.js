7aa11aad96263d6a02e143f70cd5d952876df0ad
Joanna Broks <joanna.broks@gmail.com>
2019-11-01 15:51:45 +0000

    js script moved from base to script.js file

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+            $('.collapsible').collapsible();
+            $('select').material_select();
+            $(".button-collapse").sideNav();
+        });
+        $(document).ready(function() {
+            $('select').material_select();
+        });
