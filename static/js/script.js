8b746d15ba92c43b656d3d254c98f9f3c68921a1
Joanna Broks <joanna.broks@gmail.com>
2019-11-16 12:15:53 +0000

    Function hiding and showing editable comment form fixed using parent and prevAll jQuery methods. Un-used id's removed from the viewreview.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,17 @@
-$(window).on('load', function () {
-		$("#overlay").fadeOut("slow");
+$(window).on('load', function() {
+  $("#overlay").fadeOut("slow");
 });
 
 $(document).ready(function() {
-            $('.collapsible').collapsible();
-            $('select').material_select();
-            $(".button-collapse").sideNav();
-        });
-        $(document).ready(function() {
-            $('select').material_select();
-        });
+  $('.collapsible').collapsible();
+  $('select').material_select();
+  $(".button-collapse").sideNav();
+});
+$(document).ready(function() {
+  $('select').material_select();
+});
 
-/* Submit search input on enter */    
+/* Submit search input on enter */
 $(document).ready(function() {
 
   $('.submit_on_enter').keydown(function(event) {
@@ -30,14 +30,15 @@ $("a[href='#back-to-top']").click(function() {
   return false;
 });
 
-
-
-/* Hide all (update) form as a default */
-
-$('.posted-form').hide()
-
 /* Display only the comment form which user choosed to update */
-document.getElementById('edit').addEventListener('click', function() {
-  document.getElementById('posted-form').style.display = 'block';
-  document.getElementById('card-content').style.display = 'none';
-})
+
+$(document).ready(function() {
+  /*Hide all (update) form as a default*/
+  $('.posted-form').hide();
+  $('.edit').click(function() {
+    $(this).parents('div.card-action').prevAll('.card-text').hide();
+    $(this).parents('div.card-action').prevAll('.posted-form').show();
+  });
+});
+
+
