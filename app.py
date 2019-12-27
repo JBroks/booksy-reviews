c50f7cdf2309cf96e5a45b8b9a150d29bdbce7c9
Joanna Broks <joanna.broks@gmail.com>
2019-12-27 19:08:39 +0000

    Debug mode disabled

--- app.py
+++ app.py
@@ -751,5 +751,4 @@ and where to run the application
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
-        port=int(os.environ.get('PORT')),
-        debug=True)
+        port=int(os.environ.get('PORT')))
